d7023f4aa1e154b081b585ed698ba3fad2c42a98
monopsony <39708885+monopsony@users.noreply.github.com>
2019-12-24 10:55:14 +0100

    Incremented version

--- widgets/ScrollTable.lua
+++ widgets/ScrollTable.lua
@@ -4,7 +4,7 @@ if not StdUi then
 	return
 end
 
-local module, version = 'ScrollTable', 5;
+local module, version = 'ScrollTable', 6;
 if not StdUi:UpgradeNeeded(module, version) then
 	return
 end
