a6052cf2b3fa372d5a411f3ae04f1f9ab9a14410
Xatan <glingham@gmail.com>
2022-09-15 01:37:16 +1000

    DF 10.0 SetFont() and SetHighlightTexture() arg's must not be nil.

--- widgets/Button.lua
+++ widgets/Button.lua
@@ -109,7 +109,7 @@ function StdUi:Button(parent, width, height, text, inherit)
 	local button = self:HighlightButton(parent, width, height, text, inherit)
 	button.stdUi = self;
 
-	button:SetHighlightTexture(nil);
+	button:SetHighlightTexture('');
 
 	self:ApplyBackdrop(button);
 	self:HookDisabledBackdrop(button);
